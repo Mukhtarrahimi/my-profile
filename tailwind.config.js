19820769c5e2d512de27cd0ebf20fc493a4d9b83
Mukhtarrahimi <mukhtarrahimi110@gmail.com>
2024-06-08 12:29:36 +0430

    custom fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'vazir': ['vazir','sans-serif'],
+        'yekan': ['yekan','sans-serif'],
+        'shabnam': ['shabnam','sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
